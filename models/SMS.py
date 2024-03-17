4d29c7eb7994cd58a48f1811ee00ca0588490f32
Valkyrri025 <zzy76681@gmail.com>
2024-03-17 17:59:55 +0100

    Updated voice call and SMS

---
+++ models/SMS.py
@@ -0,0 +1,19 @@
+# Download the helper library from https://www.twilio.com/docs/python/install
+import os
+from twilio.rest import Client
+
+
+# Find your Account SID and Auth Token at twilio.com/console
+# and set the environment variables. See http://twil.io/secure
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+client = Client(account_sid, auth_token)
+
+message = client.messages \
+                .create(
+                     body="Emergency message: A fall is detected in your house!",
+                     from_='+18053015048',
+                     to='+31613519355'
+                 )
+
+print(message.sid)
