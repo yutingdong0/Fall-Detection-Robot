8c5aae76a839e2803c16ad3bc2eb51ee4ea08f15
Yuting Dong <yutingdong0@gmail.com>
2024-03-21 16:01:39 +0100

    Update voice call and sms logic

--- models/CALL.py
+++ models/CALL.py
@@ -2,17 +2,18 @@
 import os
 from twilio.rest import Client
 
+class TwilioCall:
+    def call(self):
+        # Find your Account SID and Auth Token at twilio.com/console
+        # and set the environment variables. See http://twil.io/secure
+        account_sid = os.environ['TWILIO_ACCOUNT_SID']
+        auth_token = os.environ['TWILIO_AUTH_TOKEN']
+        client = Client(account_sid, auth_token)
 
-# Find your Account SID and Auth Token at twilio.com/console
-# and set the environment variables. See http://twil.io/secure
-account_sid = os.environ['TWILIO_ACCOUNT_SID']
-auth_token = os.environ['TWILIO_AUTH_TOKEN']
-client = Client(account_sid, auth_token)
+        call = client.calls.create(
+                                url='https://www.dropbox.com/scl/fi/h8gf6jriqdd5cd0pxz418/Voice_call.xml?',
+                                to='+31613519355',
+                                from_='+18053015048'
+                            )
 
-call = client.calls.create(
-                        url='https://www.dropbox.com/scl/fi/h8gf6jriqdd5cd0pxz418/Voice_call.xml?',
-                        to='+31613519355',
-                        from_='+18053015048'
-                    )
-
-print(call.sid)
+        print(call.sid)
--- models/SMS.py
+++ models/SMS.py
@@ -2,18 +2,19 @@
 import os
 from twilio.rest import Client
 
+class TwilioSMS:
+    def sms(self):
+        # Find your Account SID and Auth Token at twilio.com/console
+        # and set the environment variables. See http://twil.io/secure
+        account_sid = os.environ['TWILIO_ACCOUNT_SID']
+        auth_token = os.environ['TWILIO_AUTH_TOKEN']
+        client = Client(account_sid, auth_token)
 
-# Find your Account SID and Auth Token at twilio.com/console
-# and set the environment variables. See http://twil.io/secure
-account_sid = os.environ['TWILIO_ACCOUNT_SID']
-auth_token = os.environ['TWILIO_AUTH_TOKEN']
-client = Client(account_sid, auth_token)
+        message = client.messages \
+                        .create(
+                            body="Emergency message: A fall is detected in your house!",
+                            from_='+18053015048',
+                            to='+31613519355'
+                        )
 
-message = client.messages \
-                .create(
-                     body="Emergency message: A fall is detected in your house!",
-                     from_='+18053015048',
-                     to='+31613519355'
-                 )
-
-print(message.sid)
+        print(message.sid)
